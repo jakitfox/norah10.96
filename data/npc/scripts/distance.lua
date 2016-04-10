049d8e35185265abc8eb90ac5f3e06472ee19d7d
Genius <hkc.ots@gmail.com>
2016-04-10 11:40:44 +0200

    Naprawa NPC z bronią dystansową
    Naprawino problem z podglądem przedmiotów

--- data/npc/scripts/distance.lua
+++ data/npc/scripts/distance.lua
@@ -2,63 +2,68 @@ local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
 
-function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
-function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
-function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
+function onCreatureAppear(cid)              npcHandler:onCreatureAppear(cid)            end
+function onCreatureDisappear(cid)           npcHandler:onCreatureDisappear(cid)         end
+function onCreatureSay(cid, type, msg)      npcHandler:onCreatureSay(cid, type, msg)    end
+function onThink()                          npcHandler:onThink()                        end
 
-local shopItems = {
-	-----AMMO-----
-	-----Arrows-----
-	["arrow"] = {id = 2544, buy = 3, desc = 'arrow'},
-	["crystalline arrow"] = {id = 18304, buy = 20, desc = 'crystalline arrow'},
-	["earth arrow"] = {id = 7850, buy = 5, desc = 'earth arrow'},
-	["envenomed arrow"] = {id = 18437, buy = 12, desc = 'envenomed arrow'},
-	["flaming arrow"] = {id = 7840, buy = 5, desc = 'flaming arrow'},
-	["flash arrow"] = {id = 7838, buy = 5, desc = 'flash arrow'},
-	["onyx arrow"] = {id = 7365, buy = 7, desc = 'onyx arrow'},
-	["poison arrow"] = {id = 2545, buy = 3, desc = 'poison arrow'},
-	["shiver arrow"] = {id = 7839, buy = 5, desc = 'shiver arrow'},
-	["sniper arrow"] = {id = 7364, buy = 5, desc = 'sniper arrow'},
-	["tarsal arrow"] = {id = 15648, buy = 6, desc = 'tarsal arrow'},
-	-----Bolts-------
-	["bolt"] = {id = 2543, buy = 4, desc = 'bolt'},
-	["drill bolt"] = {id = 18436, buy = 12, desc = 'drill bolt'},
-	["piercing bolt"] = {id = 7363, buy = 5, desc = 'piercing bolt'},
-	["power bolt"] = {id = 2547, buy = 7, desc = 'power bolt'},
-	["prismatic bolt"] = {id = 18435, buy = 20, desc = 'prismatic bolt'},
-	["vortex bolt"] = {id = 15649, buy = 6, desc = 'vortex bolt'},
-	-----Bow------
-	["bow"] = {id = 2456, buy = 400, sell = 100, desc = 'bow'},
-	["composite hornbow"] = {id = 8855, sell = 25000, desc = 'composite hornbow'},
-	["elvish bow"] = {id = 7438, sell = 2000, desc = 'elvish bow'},
-	["hive bow"] = {id = 15643, sell = 28000, desc = 'hive bow'},
-	["mycological bow"] = {id = 18454, sell = 35000, desc = 'mycological bow'},
-	["silkweaver bow"] = {id = 8857, sell = 12000, desc = 'silkweaver bow'},
-	-----CrossBow------
-	["ornate crossbow"] = {id = 15644, sell = 12000, desc = 'ornate crossbow'},
-	["modified crossbow"] = {id = 8849, sell = 10000, desc = 'modified crossbow'},
-	["chain bolter"] = {id = 8850, sell = 40000, desc = 'chain bolter'},
-	["arbalest"] = {id = 5803, sell = 42000, desc = 'arbalest'},
-	["crossbow"] = {id = 2455, buy = 500, sell = 120, desc = 'crossbow'},
-	["crystal crossbow"] = {id = 18453, sell = 35000, desc = 'crystal crossbow'},
-	["ironworker"] = {id = 8853, sell = 50000, desc = 'ironworker'},
-	----Other---
-	["hunting spear"] = {id = 3965, sell = 25, desc = 'hunting spear'},
-	["assassin star"] = {id = 7368, buy = 100, desc = 'assassin star'},
-	["spear"] = {id = 2389, buy = 9, sell = 3, desc = 'spear'},
-	["royal spear"] = {id = 7378, buy = 15, desc = 'royal spear'},
-	["throwing star"] = {id = 2399, buy = 42, sell = 2, desc = 'throwing star'},
-	["throwing knife"] = {id = 2410, buy = 25, sell = 2, desc = 'throwing knife'}
-	}
 local shopModule = ShopModule:new()
 npcHandler:addModule(shopModule)
 
-if type(shopItems) == "table" then
-    for name, v in pairs(shopItems) do
-        shopModule:addBuyableItem({name}, shopItems[name].id, shopItems[name].buy, name)
-        shopModule:addSellableItem({name, name}, shopItems[name].id, shopItems[name].sell, shopItems[name].desc)
-    end
-end
+	-----Arrows-----
+	shopModule:addBuyableItem({'arrow'}, 2544, 3, 'arrow')
+	shopModule:addBuyableItem({'crystalline arrow'}, 18304, 20, 'crystalline arrow')
+	shopModule:addBuyableItem({'earth arrow'}, 7850, 5, 'earth arrow')
+	shopModule:addBuyableItem({'envenomed arrow'}, 18437, 12, 'envenomed arrow')
+	shopModule:addBuyableItem({'flaming arrow'}, 7840, 5, 'flaming arrow')
+	shopModule:addBuyableItem({'flash arrow'}, 7838, 5, 'flash arrow')
+	shopModule:addBuyableItem({'onyx arrow'}, 7365, 7, 'onyx arrow')
+	shopModule:addBuyableItem({'poison arrow'}, 2545, 3, 'poison arrow')
+	shopModule:addBuyableItem({'shiver arrow'}, 7839, 5, 'shiver arrow')
+	shopModule:addBuyableItem({'sniper arrow'}, 7364, 5, 'sniper arrow')
+	shopModule:addBuyableItem({'tarsal arrow'}, 15648, 6, 'tarsal arrow')
+	
+	-----Bolts-------
+	shopModule:addBuyableItem({'bolt'}, 2543, 4, 'bolt')
+	shopModule:addBuyableItem({'drill bolt'}, 18436, 12, 'drill bolt')
+	shopModule:addBuyableItem({'piercing bolt'}, 7363, 5, 'piercing bolt')
+	shopModule:addBuyableItem({'power bolt'}, 2547, 7, 'power bolt')
+	shopModule:addBuyableItem({'prismatic bolt'}, 18435, 20, 'prismatic bolt')
+	shopModule:addBuyableItem({'vortex bolt'}, 15649, 6, 'vortex bolt')
+
+	-----Bow------
+	shopModule:addBuyableItem({'bow'}, 2456, 400, 'bow')
+	
+	shopModule:addSellableItem({'bow'}, 2456, 100, 'bow')
+	shopModule:addSellableItem({'composite hornbow'}, 8855, 25000, 'composite hornbow')
+	shopModule:addSellableItem({'elvish bow'}, 7438, 2000, 'elvish bow')
+	shopModule:addSellableItem({'hive bow'}, 15643, 28000, 'hive bow')
+	shopModule:addSellableItem({'mycological bow'}, 18454, 35000, 'mycological bow')
+	shopModule:addSellableItem({'silkweaver bow'}, 8857, 12000, 'silkweaver bow')
+
+	-----CrossBow------
+	shopModule:addBuyableItem({'crossbow'}, 2455, 500, 'crossbow')
+	
+	shopModule:addSellableItem({'crossbow'}, 2455, 120, 'crossbow')
+	shopModule:addSellableItem({'ornate crossbow'}, 15644, 12000, 'ornate crossbow')
+	shopModule:addSellableItem({'modified crossbow'}, 8849, 10000, 'modified crossbow')
+	shopModule:addSellableItem({'chain bolter'}, 8850, 40000, 'chain bolter')
+	shopModule:addSellableItem({'arbalest'}, 5803, 42000, 'arbalest')
+	shopModule:addSellableItem({'crystal crossbow'}, 18453, 35000, 'crystal crossbow')
+	shopModule:addSellableItem({'ironworker'}, 18453, 50000, 'ironworker')
+
+	----Other---
+	shopModule:addBuyableItem({'assassin star'}, 7368, 100, 'assassin star')
+	shopModule:addBuyableItem({'spear'}, 2389, 9, 'spear')
+	shopModule:addBuyableItem({'royal spear'}, 7378, 15, 'royal spear')
+	shopModule:addBuyableItem({'throwing star'}, 2399, 42, 'throwing star')
+	shopModule:addBuyableItem({'throwing knife'}, 2410, 25, 'throwing knife')
+	
+	shopModule:addSellableItem({'hunting spear'}, 3965, 25, 'hunting spear')
+	shopModule:addSellableItem({'spear'}, 2389, 3, 'spear')
+	shopModule:addSellableItem({'throwing star'}, 2399, 2, 'throwing star')
+	shopModule:addSellableItem({'throwing knife'}, 2410, 2, 'throwing knife')
+
 
 npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
 npcHandler:setMessage(MESSAGE_GREET, "Welcome to my legal shop.")
