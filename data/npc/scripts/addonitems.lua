a008a1576160e9eabcab394d92c123fd318eff9e
Genius <hkc.ots@gmail.com>
2016-09-11 14:04:47 +0200

    Poprawki v1.3

--- data/npc/scripts/addonitems.lua
+++ data/npc/scripts/addonitems.lua
@@ -2,65 +2,65 @@ local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
 
-function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
-function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
-function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
+function onCreatureAppear(cid)              npcHandler:onCreatureAppear(cid)            end
+function onCreatureDisappear(cid)           npcHandler:onCreatureDisappear(cid)         end
+function onCreatureSay(cid, type, msg)      npcHandler:onCreatureSay(cid, type, msg)    end
+function onThink()                          npcHandler:onThink()                        end
 
-local shopItems = {
-	["ape fur"] = {id = 5883, sell = 120, desc = 'ape fur'},
-	["bat wing"] = {id = 5894, sell = 50, desc = 'bat wing'},
-	["bear paw"] = {id = 5896, sell = 100, desc = 'bear paw'},
-	["behemoth claw"] = {id = 5930, sell = 2000, desc = 'behemoth claw'},
-	["bonelord eye"] = {id = 5898, sell = 80, desc = 'bonelord eye'},
-	["chicken feather"] = {id = 5890, sell = 30, desc = 'chicken feather'},
-	["demon dust"] = {id = 5906, sell = 300, desc = 'demon dust'},
-	["dragon claw"] = {id = 10020, sell = 8000, desc = 'dragon claw'},
-	["fish fin"] = {id = 5895, sell = 150, desc = 'fish fin'},
-	["spider silk"] = {id = 5879, sell = 100, desc = 'spider silk'},
-	["hardened bone"] = {id = 5925, sell = 70, desc = 'hardened bone'},
-	["heaven blossom"] = {id = 5921, sell = 50, desc = 'heaven blossom'},
-	["holy orchid"] = {id = 5922, sell = 90, desc = 'holy orchid'},
-	["honeycomb"] = {id = 5902, sell = 40, desc = 'honeycomb'},
-	["huge chunk of crude iron"] = {id = 5892, sell = 15000, desc = 'huge chunk of crude iron'},
-	["iron ore"] = {id = 5880, sell = 500, desc = 'iron ore'},
-	["lizard leather"] = {id = 5876, sell = 150, desc = 'lizard leather'},
-	["lizard scale"] = {id = 5881, sell = 120, desc = 'lizard scale'},
-	["minotaur leather"] = {id = 5878, sell = 80, desc = 'minotaur leather'},
-	["nose ring"] = {id = 5804, sell = 2000, desc = 'nose ring'},
-	["perfect behemoth fang"] = {id = 5893, sell = 250, desc = 'perfect behemoth fang'},
-	["red dragon leather"] = {id = 5948, sell = 200, desc = 'red dragon leather'},
-	["red dragon scale"] = {id = 5882, sell = 200, desc = 'red dragon scale'},
-	["spool of yarn"] = {id = 5886, sell = 1000, desc = 'spool of yarn'},
-	["turtle shell"] = {id = 5899, sell = 90, desc = 'turtle shell'},
-	["vampire dust"] = {id = 5905, sell = 100, desc = 'vampire dust'},
-	["wolf paw"] = {id = 5897, sell = 70, desc = 'wolf paw'},
-	["blue piece of cloth"] = {id = 5912, sell = 200, desc = 'blue piece of cloth'},
-	["brown piece of cloth"] = {id = 5913, sell = 100, desc = 'brown piece of cloth'},
-	["green piece of cloth"] = {id = 5910, sell = 200, desc = 'green piece of cloth'},
-	["red piece of cloth"] = {id = 5911, sell = 300, desc = 'red piece of cloth'},
-	["white piece of cloth"] = {id = 5909, sell = 100, desc = 'white piece of cloth'},
-	["yellow piece of cloth"] = {id = 5914, sell = 150, desc = 'yellow piece of cloth'},
-	["enchanted chicken wing"] = {id = 5891, sell = 20000, desc = 'enchanted chicken wing'},
-	["spirit container"] = {id = 5884, sell = 40000, desc = 'spirit container'},
-	["magic sulphur"] = {id = 5904, sell = 8000, desc = 'magic sulphur'},
-	["piece of draconian steel"] = {id = 5889, sell = 3000, desc = 'piece of draconian steel'},
-	["piece of hell steel"] = {id = 5888, sell = 500, desc = 'piece of hell steel'},
-	["piece of royal steel"] = {id = 5887, sell = 10000, desc = 'piece of royal steel'},
-	["sniper gloves"] = {id = 5875, sell = 2000, desc = 'sniper gloves'},
-	["soul stone"] = {id = 5809, sell = 6000, desc = 'soul stone'},
-	["flask of warrior's sweat"] = {id = 5885, sell = 10000, desc = 'flask of warrior's sweat'},
-	["royal tapestry"] = {id = 9958, buy = 560, sell = 1000, desc = 'royal tapestry'}
-	}
-	
 local shopModule = ShopModule:new()
 npcHandler:addModule(shopModule)
 
-if type(shopItems) == "table" then
-    for name, v in pairs(shopItems) do
-        shopModule:addBuyableItem({name}, shopItems[name].id, shopItems[name].buy, name)
-        shopModule:addSellableItem({name, name}, shopItems[name].id, shopItems[name].sell, shopItems[name].desc)
-    end
-end
+
+	shopModule:addSellableItem({'shard'}, 7290, 2000, 'shard')
+	shopModule:addSellableItem({'red dragon scale'}, 5882, 200, 'red dragon scale')
+	shopModule:addSellableItem({'red dragon leather'}, 5948, 200, 'red dragon leather')
+	shopModule:addSellableItem({'lizard scale'}, 5881, 120, 'lizard scale')
+	shopModule:addSellableItem({'lizard leather'}, 5876, 150, 'lizard leather')
+	shopModule:addSellableItem({'green dragon scale'}, 5920, 100, 'green dragon scale')
+	shopModule:addSellableItem({'green dragon leather'}, 5877, 100, 'green dragon leather')
+	shopModule:addSellableItem({'ape fur'}, 5883, 120, 'ape fur')
+	shopModule:addSellableItem({'bat wing'}, 5894, 50, 'bat wing')
+	shopModule:addSellableItem({'bear paw'}, 5896, 100, 'bear paw')
+	shopModule:addSellableItem({'behemoth claw'}, 5930, 2000, 'behemoth claw')
+	shopModule:addSellableItem({'bonelord eye'}, 5898, 80, 'bonelord eye')
+	shopModule:addSellableItem({'chicken feather'}, 5890, 30, 'chicken feather')
+	shopModule:addSellableItem({'demon dust'}, 5906, 300, 'demon dust')
+	shopModule:addSellableItem({'dragon claw'}, 10020, 8000, 'dragon claw')
+	shopModule:addSellableItem({'fish fin'}, 5895, 150, 'fish fin')
+	shopModule:addSellableItem({'spider silk'}, 5879, 100, 'spider silk')
+	shopModule:addSellableItem({'hardened bone'}, 5925, 70, 'hardened bone')
+	shopModule:addSellableItem({'heaven blossom'}, 5921, 50, 'heaven blossom')
+	shopModule:addSellableItem({'holy orchid'}, 5922, 90, 'holy orchid')
+	shopModule:addSellableItem({'honeycomb'}, 5902, 40, 'honeycomb')
+	shopModule:addSellableItem({'huge chunk of crude iron'}, 5892, 15000, 'huge chunk of crude iron')
+	shopModule:addSellableItem({'iron ore'}, 5880, 500, 'iron ore')
+	shopModule:addSellableItem({'lizard leather'}, 5876, 150, 'lizard leather')
+	shopModule:addSellableItem({'lizard scale'}, 5881, 120, 'lizard scale')
+	shopModule:addSellableItem({'minotaur leather'}, 5878, 80, '"minotaur leather')
+	shopModule:addSellableItem({'nose ring'}, 5804, 2000, 'nose ring')
+	shopModule:addSellableItem({'perfect behemoth fang'}, 5893, 250, 'perfect behemoth fang')
+	shopModule:addSellableItem({'red dragon leather'}, 5948, 200, 'red dragon leather')
+	shopModule:addSellableItem({'red dragon scale'}, 5882, 200, 'red dragon scale')
+	shopModule:addSellableItem({'spool of yarn'}, 5886, 1000, 'spool of yarn')
+	shopModule:addSellableItem({'turtle shell'}, 5899, 90, 'turtle shell')
+	shopModule:addSellableItem({'vampire dust'}, 5905, 90, 'vampire dust')
+	shopModule:addSellableItem({'wolf paw'}, 5899, 90, 'wolf paw')
+	shopModule:addSellableItem({'blue piece of cloth'}, 5912, 200, 'blue piece of cloth')
+	shopModule:addSellableItem({'brown piece of cloth'}, 5913, 100, 'brown piece of cloth')
+	shopModule:addSellableItem({'green piece of cloth'}, 5910, 200, 'green piece of cloth')
+	shopModule:addSellableItem({'red piece of cloth'}, 5911, 300, 'red piece of cloth')
+	shopModule:addSellableItem({'white piece of cloth'}, 5909, 100, 'white piece of cloth')
+	shopModule:addSellableItem({'yellow piece of cloth'}, 5914, 150, 'yellow piece of cloth')
+	shopModule:addSellableItem({'enchanted chicken wing'}, 5891, 20000, 'enchanted chicken wing')
+	shopModule:addSellableItem({'spirit container'}, 5884, 40000, 'spirit container')
+	shopModule:addSellableItem({'magic sulphur'}, 5904, 8000, 'magic sulphur')
+	shopModule:addSellableItem({'piece of draconian steel'}, 5889, 3000, 'piece of draconian steel')
+	shopModule:addSellableItem({'piece of hell steel'}, 5888, 500, 'piece of hell steel')
+	shopModule:addSellableItem({'piece of royal steel'}, 5887, 10000, 'piece of royal steel')
+	shopModule:addSellableItem({'sniper gloves'}, 5875, 2000, 'sniper gloves')
+	shopModule:addSellableItem({'soul stone'}, 5809, 6000, 'soul stone')
+	shopModule:addSellableItem({'flask of warrior\'s sweat'}, 5885, 10000, 'flask of warrior\'s sweat')
+	shopModule:addSellableItem({'royal tapestry'}, 9958, 1000, 'royal tapestry')
 
 npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
 npcHandler:setMessage(MESSAGE_GREET, "Welcome to my legal shop.")
